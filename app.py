99771533629566bd75b1a96f00bc0a3a24f75b11
JIHAE <pimangnono@gmail.com>
2024-06-30 23:19:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
 from dotenv import load_dotenv
 import re
+import os 
 
 load_dotenv()
+
+# Now you can access your environment variables
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
 # 1. Vectorize the abstracts data
 loader = CSVLoader("output.csv")
