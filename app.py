afe5c8a1768012ffc111f9b5810a34e92d504881
JIHAE <pimangnono@gmail.com>
2024-06-30 22:36:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import re
 load_dotenv()
 
 # 1. Vectorize the abstracts data
-loader = CSVLoader("/Users/pjihae/Documents/GitHub/output.csv")
+loader = CSVLoader("GitHub/output.csv")
 documents = loader.load()
 
 embeddings = OpenAIEmbeddings()
